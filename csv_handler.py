2f7986f88e0e4b0dd8d5c95d362bdd30916dd356
pushpendra1809 <168222747+pushpendra1809@users.noreply.github.com>
2025-03-12 23:05:44 +0530

    Update csv_handler.py

--- csv_handler.py
+++ csv_handler.py
@@ -2,11 +2,7 @@
 import pandas as pd
 
 def load_csv(file):
-    """
-    Load and validate a CSV file.
-    :param file: Uploaded file object
-    :return: DataFrame if successful, error message if failed
-    """
+   
     try:
         df = pd.read_csv(file.name)
         return df
